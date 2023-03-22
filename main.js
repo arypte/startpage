26c5ef2a517c31a17b714f614ec56e4c3bf307f3
arypte <wjs505@gmail.com>
2023-03-22 18:02:55 +0900

    실시간으로 갱신되는 시계

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-const time = document.querySelector(".time");
+function getTime() {
+  const time = document.querySelector(".time");
 
-const newDate = new Date();
+  const newDate = new Date();
 
-const hours = newDate.getHours();
-const minutes = newDate.getMinutes();
-const seconds = newDate.getSeconds();
+  const hours = String(newDate.getHours()).padStart(2, "0");
+  const minutes = String(newDate.getMinutes()).padStart(2, "0");
+  const seconds = String(newDate.getSeconds()).padStart(2, "0");
 
-time.innerText = hours + ":" + minutes + ":" + seconds;
+  if (seconds.toString().length === 1) {
+    seconds = "0" + seconds;
+  }
 
-console.log(time);
+  // time.innerText = hours + ":" + minutes + ":" + seconds;
+  time.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getTime();
+
+setInterval(getTime, 1000);
