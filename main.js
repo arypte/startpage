b04462880d9e176ab5c97230fa916dee33316f25
arypte <wjs505@gmail.com>
2023-03-27 16:46:43 +0900

    startpage_final
    startpage_final

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const QUOTES = "quotes";
+
 function getTime() {
   const time = document.querySelector(".time");
 
@@ -12,9 +14,115 @@ function getTime() {
   }
 
   // time.innerText = hours + ":" + minutes + ":" + seconds;
-  time.innerText = `${hours}:${minutes}:${seconds}`;
+  time.innerText = `${hours} : ${minutes} : ${seconds}`;
 }
 
 getTime();
-
 setInterval(getTime, 1000);
+
+function getQuotes() {
+  const quotesMsg = document.querySelector(".quotesMsg");
+  let savedQuotes = localStorage.getItem(QUOTES);
+
+  if (!savedQuotes) {
+    localStorage.setItem(
+      QUOTES,
+      JSON.stringify([
+        "열심히 살지맙시다.",
+        "그래도 열심히 살아야지.",
+        "열심히 살면 뭐해~",
+        "열심히 살면 반드시 빛이 온다.",
+      ])
+    );
+
+    savedQuotes = localStorage.getItem(QUOTES);
+  }
+
+  let quotesArray = JSON.parse(savedQuotes);
+
+  quotesMsg.innerText =
+    quotesArray[Math.floor(Math.random() * quotesArray.length)];
+}
+
+getQuotes();
+
+function onClickAdd() {
+  const newQuotes = document.querySelector(".newQuotes");
+
+  newQuotes.style.display = "inline-block";
+}
+
+function onClickRegist() {
+  const quotesMsg = document.querySelector(".quotesMsg");
+  const newQuotes = document.querySelector(".newQuotes");
+  const newQuotesInput = document.querySelector(".newQuotesInput");
+
+  if (!newQuotesInput.value) {
+    return;
+  }
+
+  let savedQuotes = localStorage.getItem(QUOTES);
+
+  let quotesArray = JSON.parse(savedQuotes);
+  quotesArray.push(newQuotesInput.value);
+
+  localStorage.setItem(QUOTES, JSON.stringify(quotesArray));
+
+  quotesMsg.innerHTML = `<span style="color:red;">${newQuotesInput.value}</span>`;
+  newQuotes.style.display = "none";
+}
+
+let isLoading = false;
+
+async function onClickSearch() {
+  // async 비동기 , await 사용하기 위해
+  const searchInput = document.querySelector(".searchInput");
+  const searchResult = document.querySelector(".searchResult");
+
+  if (!searchInput.value) return;
+  if (!isLoading) return;
+
+  isLoading = true;
+  const question = searchInput.value;
+
+  searchInput.value = "검색 중 입니다... 잠시만 기다려주세요.";
+
+  // 프론트엔드에서 백엔드로 보내는 소스
+  const response = await axios.post(
+    /* get 요청 post 요청.
+    백엔드에서 post요청을 받게 설정을 해둬서 post로
+    await 잠시 기다려달리
+    */
+    "https://holy-fire-2749.fly.dev/chat",
+    {
+      question,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer BLOCKCHAINSCHOOL3",
+      },
+    }
+  );
+
+  if (response.status == 200) {
+    searchResult.style.display = "inline";
+    searchResult.innerText = response.data.choices[0].message.content;
+  }
+
+  searchInput.value = "";
+  isLoading = false;
+}
+
+function onClickToggle(value) {
+  const nft = document.querySelector(".nft");
+  const nftView = document.querySelector(".nftView");
+
+  if (value) {
+    nft.style.display = "inline-block";
+    nftView.style.display = "none";
+  } else {
+    nft.style.display = "none";
+    nftView.style.display = "inline-block";
+  }
+}
